83cbf13e484d0d0848bb2616ce9fd1c9961cdd6e
khaliuss <khalidkhushmadov@gmail.com>
2026-03-24 07:28:03 +0300

    Service 3: Notification added

--- Services/app/src/main/java/com/example/services/MainActivity.kt
+++ Services/app/src/main/java/com/example/services/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,12 +19,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                101
+            )
+        }
+
         binding.simpleService.setOnClickListener {
-            startService(MyService.newIntent(this,5))
+            startService(MyService.newIntent(this, 5))
+        }
+
+        binding.foregroundService.setOnClickListener {
+            createNotification()
+        }
+
+    }
+
+    private fun createNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChanel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            notificationManager.createNotificationChannel(notificationChanel)
         }
 
 
 
+        val notification = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setContentTitle("My Title Jeee")
+            .setContentText("Some text")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+
+
+        notificationManager.notify(1, notification)
+    }
+
+    companion object {
+
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 
 
